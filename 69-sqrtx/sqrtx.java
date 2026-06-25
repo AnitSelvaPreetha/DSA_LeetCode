a80dc65e60b706d94873b1b22a1d346f8ef098f8
Anit Selva Preetha M V <anitselvaaas666@gmail.com>
2026-06-25 21:56:58 +0530

    Time: 1 ms (97.81%) | Memory: 42.9 MB (8.34%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int mySqrt(int x) {
+
+if(x<2)
+return x;
+
+        int start=1;
+        int end=x/2;
+        int ans=0;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(x/mid==mid)
+            {
+                return mid;
+            }
+            else if(x/mid>mid)
+            {
+                ans=mid;
+            start=mid+1;
+                
+            }
+            else
+            {
+            end=mid-1;
+            }
+        }
+
+    
+        return ans;
+    }
+}
