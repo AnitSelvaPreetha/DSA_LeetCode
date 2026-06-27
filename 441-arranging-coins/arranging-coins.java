ea4c999f9ddfa3a7e9bff63acafc65496bba2601
Anit Selva Preetha M V <anitselvaaas666@gmail.com>
2026-06-28 03:52:03 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (8.87%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int arrangeCoins(int n) {
+
+       long start=0;
+       long end=n;
+
+       while(start<=end)
+       {
+        long mid=start+(end-start)/2;
+        long coins=mid*(mid+1)/2;
+
+        if(n==coins)
+        return (int)mid;
+
+        else if(coins>n)
+        end=mid-1;
+
+        else
+        start=mid+1;
+
+       }
+        return (int)end;
+    }
+}
